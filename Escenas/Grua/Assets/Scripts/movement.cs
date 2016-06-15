4a9e1d902f43d45f86e9740e7bf0116c6abd0bdf
Rafael Vargas <rafavch@yahoo.com>
2016-06-15 00:37:53 -0500

    Escenas base
    Escenas base de grua y box

---
+++ Escenas/Grua/Assets/Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	GameObject camara;
+	float yRotate;
+	//float speed = 3;
+
+	void Start () {
+		
+		//UnityEngine.GameObject.Find("yRotation");
+		camara = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		print ("X: " + camara.transform.eulerAngles.x + "  Y: " + camara.transform.eulerAngles.y + "  Z: " + camara.transform.eulerAngles.z);
+
+		if (yRotate != camara.transform.eulerAngles.y) {
+			yRotate = camara.transform.eulerAngles.y;
+			//transform.Rotate (new Vector3 (0, yRotate - 90, 0) * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0, camara.transform.eulerAngles.y - 90, 0); 
+		}
+
+		//transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+		//transform.Translate(0, 0, 0);
+		//transform.Translate(0, 0, 0, Space.World);
+		//angles = InputTracking.GetLocalRotation (VRNode.CenterEye);
+
+	}
+}
